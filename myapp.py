c4f28a69b3d7980e050e8d136a4bca4c73501487
elfrnx <114773516+elfrnx@users.noreply.github.com>
2024-01-30 15:28:17 +0100

    Add files via upload

--- myapp.py
+++ myapp.py
@@ -1,31 +1,4 @@
 import streamlit as st
-
-
-st.title("Streamlit Cheat Sheet")
-st.write("This is my first Streamlit app.")
-
-# Header for the first section
-st.header("Markdown")
-
-# Write some markdown
-st.write("## This is some Markdown text.")
-
-# Header for the second section
-st.header("Code")
-
-# Write some code
-st.code("""
-for i in range(8):
-  print("nlahtanla")
-""")
-
-# Header for the third section
-st.header("Camera")
-
-# Use the camera widget to take a picture
-st.camera_input("Take a picture")
-
-
 import cv2
 from keras.models import load_model
 from PIL import Image, ImageOps
